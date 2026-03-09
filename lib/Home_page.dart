6ee0f5a7fb1b0fbb7b9e6078e6902b94de434cab
lakshita verma <vermalakshita63@gmail.com>
2026-03-09 13:29:19 +0530

    h

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -11,8 +11,7 @@ class HomePage extends StatelessWidget {
       ),
 
       body: Center(
-        child: Container(
-          child: Text("WELCOME to $days of Flutter by $name ")),
+        child: Container(child: Text("WELCOME to $days of Flutter by $name ")),
       ),
       drawer:Drawer(),
     );
