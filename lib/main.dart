5056d69eb9f3a0c3b59f33a0ac1857949a0d807f
lakshita verma <vermalakshita63@gmail.com>
2026-03-09 10:35:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key); //constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Material(child: Container(child: Text("Hello World"))),
+      ), //container
+    ); //MaterialApp
+  }
+}
