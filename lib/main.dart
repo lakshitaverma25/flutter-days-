1b5bd445c285c25230358fca0d9afe719769f43e
lakshita verma <vermalakshita63@gmail.com>
2026-03-09 13:27:57 +0530

    h

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'Home_page.dart';
 
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   
+  //const MyApp({Key? key}) : super(key: key); //constructor
 
   @override
   Widget build(BuildContext context) {
