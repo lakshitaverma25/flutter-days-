3eeba84313230d8ad09404e35057089b8962c793
lakshita verma <vermalakshita63@gmail.com>
2026-03-09 13:43:27 +0530

    h

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -7,13 +7,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-
+        title: Text("Catalog App")
+        ),
       body: Center(
-        child: Container(child: Text("WELCOME to $days of Flutter by $name ")),
+        child: Container(child: Text("WELCOME to $days of Flutter by $name ")
+        ),
       ),
-      drawer:Drawer(),
+      drawer: Drawer(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'Home_page.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   //const MyApp({Key? key}) : super(key: key); //constructor
 
   @override
