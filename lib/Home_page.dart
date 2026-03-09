047f75872b9bd1bac391dff673d0dde5b1f605d0
lakshita verma <vermalakshita63@gmail.com>
2026-03-09 12:28:19 +0530

    h

---
+++ lib/Home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Codepur";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(child: Text("WELCOME to $days of Flutter by $name ")),
+      ),
+      drawer:Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
+import 'Home_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); //constructor
+  //const MyApp({Key? key}) : super(key: key); //constructor
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("WELCOME to $days of Flutter")
-            )
-            ),
-      ),
-    );
+      home: HomePage(),
+    ); //MaterialApp is a widget that wraps a number of widgets that are commonly required for material design applications.
   }
 }
